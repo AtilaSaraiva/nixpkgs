5e74ff1326e79c7fab94e7409d8345940f2b4303
Átila Saraiva <atilasaraiva@gmail.com>
2022-06-13 19:33:26 -0300

    WIP

--- pkgs/development/tools/build-managers/fortran-fpm/default.nix
+++ pkgs/development/tools/build-managers/fortran-fpm/default.nix
@@ -1,25 +1,47 @@
-{ stdenv, lib, fetchurl, gfortran }:
+{ stdenv, lib, fetchFromGitHub, fetchurl, gfortran, git, toml-f }:
 
+let
+  M_CLI2 = fetchFromGitHub {
+    owner = "urbanjost";
+    repo = "M_CLI2";
+    rev = "ea6bbffc1c2fb0885e994d37ccf0029c99b19f24";
+    sha256 = "sha256-EDYTy1XbQQRt2ga9egzrIBhUwjfe89cy7+gW3Yyg86U=";
+  };
+in
 stdenv.mkDerivation rec {
   pname = "fpm";
   version = "0.5.0";
 
-  src = fetchurl {
+  fpm_bootstrap = fetchurl {
     url = "https://github.com/fortran-lang/fpm/releases/download/v${version}/fpm-${version}.F90";
     sha256 = "sha256-U7uk09Cdh11RPtQwmj3v5BT6cn283rOOT9zwlOGcAlc=";
   };
 
-  dontUnpack = true;
+  src = fetchFromGitHub {
+    owner = "fortran-lang";
+    repo = "fpm";
+    rev = "v${version}";
+    sha256 = "sha256-j6jKmlTjsgxNBMk874sb2SBZr9b408/Cx8f4TzCVgTI=";
+  };
 
-  nativeBuildInputs = [ gfortran ];
+  patches = [ ./test.patch ];
 
-  buildPhase = ''
-    gfortran ${src} -o fpm
+  postPatch = ''
+    cp -r ${M_CLI2} M_CLI2
+  '';
+
+  nativeBuildInputs = [ gfortran git toml-f ];
+
+  preConfigure = ''
+    export HOME=$PWD
   '';
 
   installPhase = ''
-    install -Dm755 -t $out/bin/ fpm
+    gfortran ${fpm_bootstrap} -o fpm
+    FFLAGS="-g -fbacktrace -O3"
+    ./fpm install --compiler gfortran --flag "$FFLAGS" --prefix $out
   '';
+    #install -Dm755 -t $out/bin/ fpm
 
   meta = with lib; {
     description = "Fortran Package Manager (fpm)";
